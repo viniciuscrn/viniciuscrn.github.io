4e1f082d355fc86a899569f04435b94cd66456bd
Vinicius Marques <viniciuscrn@gmail.com>
2024-09-17 17:55:01 -0300

    Percorrendo arrays - PNT

--- aulas/pnt/aula_8/scripts.js
+++ aulas/pnt/aula_8/scripts.js
@@ -1,1 +1,42 @@
-console.log("Aqui")
+const cores = []
+
+document.getElementById("adicionar").addEventListener("click", function () {
+    cor = document.getElementById("cor").value
+    cores.push(cor)
+    exibir()
+    document.getElementById("cor").value = ""
+    document.getElementById("cor").focus()
+})
+
+function exibir() {
+    document.getElementById("lista_cores").innerHTML = ""
+    cores.forEach(function (element, i) {
+        document.getElementById("lista_cores").innerHTML += "<li>" + element + " <a href='#' onclick='excluir(" + i + ")' id='excluir'>remover</a></li>"
+    })
+}
+
+function excluir(id) {
+    delete cores[id]
+    exibir()
+}
+
+function ordenar_crescente() {
+    cores.sort()
+    exibir()
+}
+
+function ordenar_decrescente() {
+    cores.reverse()
+    exibir()
+}
+
+function pesquisar() {
+    let text = document.getElementById("pesquisa").value
+    coresFilter = cores.filter(function (element) {
+        return cores.includes(element)
+    })
+
+    coresFilter.forEach(element => {
+        document.getElementById("exibir_pesquisa").innerHTML += "<p>" + element + "</p>"
+    })
+}
