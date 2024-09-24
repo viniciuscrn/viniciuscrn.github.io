ca6312586fcfe7f7dc1602888f2832efb311b995
Vinicius Marques <viniciuscrn@gmail.com>
2024-09-24 17:47:47 -0300

    Atualização código controle de empregados

--- aulas/pnt/aula_9/js/scripts.js
+++ aulas/pnt/aula_9/js/scripts.js
@@ -1,6 +1,6 @@
 //Dados dos Empregados
 
-const empregados = [
+let empregados = [
     { nome: "Alice Silva", idade: 28, cargo: "Desenvolvedora", salario: 3500, cidade: "São Paulo", estado: "SP" },
     { nome: "Bruno Pereira", idade: 34, cargo: "Gerente", salario: 7200, cidade: "Rio de Janeiro", estado: "RJ" },
     { nome: "Carlos Almeida", idade: 30, cargo: "Analista", salario: 5200, cidade: "Belo Horizonte", estado: "MG" },
@@ -22,3 +22,79 @@ const empregados = [
     { nome: "Thiago Cavalcanti", idade: 41, cargo: "Gerente", salario: 8200, cidade: "Aracaju", estado: "SE" },
     { nome: "Ursula Rocha", idade: 28, cargo: "Suporte", salario: 3200, cidade: "Maceió", estado: "AL" }
 ];
+
+exibir()
+
+function exibir() {
+    document.getElementById("itens_tbody").innerHTML = ''
+    empregados.forEach((element, i) => {
+        let html = `
+        <tr>
+            <td>${element.nome}</td>
+            <td>${element.idade}</td>
+            <td>${element.cargo}</td>
+            <td>${element.salario}</td>
+            <td>${element.cidade}</td>
+            <td>${element.estado}</td>
+            <td>
+                <a href="#" class="text-info" title="Detalhar" onclick="detalhar(${i})" data-bs-toggle="modal" data-bs-target="#detalharModal"><i class="fas fa-eye"></i></a>
+                <a href="#" class="text-warning ms-3" title="Editar" onclick="editar(${i})" data-bs-toggle="modal" data-bs-target="#editarModal"><i class="fas fa-edit"></i></a>
+                <a href="#" class="text-danger ms-3" title="Excluir" onclick="excluir(${i})"><i class="fas fa-trash-alt"></i></a>
+            </td>
+        </tr>`
+        document.getElementById("itens_tbody").innerHTML += html
+    })
+}
+
+function detalhar(i) {
+    const empregado = empregados[i]
+    let detalhes = `
+    <p>Nome: <strong> ${empregado.nome}</strong></p>
+    <p>Idade: <strong> ${empregado.idade}</strong></p>
+    <p>Cargo: <strong> ${empregado.cargo}</strong></p>
+    <p>Salário: <strong> ${empregado.salario}</strong></p>
+    <p>Cidade: <strong> ${empregado.cidade}</strong></p>
+    <p>Estado: <strong> ${empregado.estado}</strong></p>
+    `
+    document.getElementById("detalhesEmpregado").innerHTML = detalhes
+}
+
+function editar(i) {
+    const empregado = empregados[i]
+    document.getElementById("indice").value = i
+    document.getElementById("nome").value = empregado.nome
+    document.getElementById("idade").value = empregado.idade
+    document.getElementById("cargo").value = empregado.cargo
+    document.getElementById("salario").value = empregado.salario
+    document.getElementById("cidade").value = empregado.cidade
+    document.getElementById("estado").value = empregado.estado
+}
+
+function atualizar() {
+    let i = document.getElementById("indice").value
+
+    let empregado = empregados[i]
+    empregado.nome = document.getElementById("nome").value
+    empregado.idade = document.getElementById("idade").value
+    empregado.cargo = document.getElementById("cargo").value
+    empregado.salario = document.getElementById("salario").value
+    empregado.cidade = document.getElementById("cidade").value
+    empregado.estado = document.getElementById("estado").value
+
+    exibir()
+}
+
+function excluir(i) {
+    empregados.splice(i, 1)
+    console.log(empregados)
+    exibir()
+}
+
+
+const totalSalarios = empregados.reduce((total, emp) => {
+    return total += emp.salario
+}, 0)
+
+
+
+console.log(totalSalarios)
